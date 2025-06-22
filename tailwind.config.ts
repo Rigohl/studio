5b16abcf0697bde108eaf9d3b9e5f9d90e861760
Rigohl <rigobertohula@gmail.com>
2025-06-22 16:28:58 -0600

    1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './mi-web-cancion/src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './mi-web-cancion/src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
